be39489edf5b57fe0061bd58342daae85b7d8c03
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-08-02 10:03:45 +0900

    release

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
-
 # http://localhost:5000/bill-of-material/list/ownerProductionPlantBusinessPartner
 
+generate-json:
+	node mergeMockJson.js
+
+json-server-watch:
+	npm run json-server:server
--- mergeMockJson.js
+++ mergeMockJson.js
@@ -1,20 +1,23 @@
-
 const fs = require('fs');
 const path = require('path');
 const root = path.resolve('./', 'api');
 
 let apiRes = {}
 
-const allFile = fs.readdirSync(root, { withFileTypes: false })
+const allFiles = fs.readdirSync(root, { withFileTypes: false })
 
-allFile.forEach((file) => {
+allFiles.forEach((file) => {
+    console.log(`try ${file}`);
+
     const endpoint = path.basename(file, path.extname(file));
     // apiRes[endpoint] = JSON.parse(fs.readFileSync(root + '/' + file, 'utf-8'));
     // apiRes = JSON.parse(fs.readFileSync(root + '/' + file, 'utf-8'));
     const b = JSON.parse(fs.readFileSync(root + '/' + file, 'utf-8'));
-    apiRes = Object.assign(apiRes, b)
+    apiRes = Object.assign(apiRes, b);
+
+    console.log(`complete ${file}`);
 })
 
-fs.writeFileSync(root + '/../mock.json', JSON.stringify(apiRes), function (err) {
+fs.writeFileSync(root + '/../mock.json', JSON.stringify(apiRes), (err) => {
     if (err) throw err;
 });
